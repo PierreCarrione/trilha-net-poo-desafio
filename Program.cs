5e93ae8e31052c084a7fd15250867ca47f3d6ea1
PierreCarrione <pierre.rossi@hotmail.com>
2025-01-16 10:52:21 -0300

    Implementado a relação de herança entre as classes e seus métodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("21999999999", "3110", "SAjiS5fgSZX", 1);
+Iphone iphone = new Iphone("218888888889", "15 pro max", "SFUH154SF", 6);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Jogo da cobrinha");
+iphone.Ligar();
+iphone.InstalarAplicativo("Siri");
